56647ac3ca8aabc25e8fdc8cd426997b4138bd0f
ginish314 <ginishkumardurg@gmail.com>
2025-07-28 16:08:33 +0530

    Add files via upload

---
+++ test_greeter.py
@@ -0,0 +1,11 @@
+import unittest
+from greeter import Greeter
+
+class TestGreeter(unittest.TestCase):
+    
+    def test_say_hello(self):
+        g = Greeter("Siddharth")
+        self.assertEqual(g.say_hello(), "Hello, Siddharth")
+
+if __name__ == "__main__":
+    unittest.main()
